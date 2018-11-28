097e5fa85c2c3bb64676c19dbc06307998ca3cd8
emgamble <eg913@pvsd.org>
2018-11-27 21:15:38 -0500

    whatever

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int cirCount = 0;
+        int sqrCount = 0;
+        MonteCarlo mcObj = new MonteCarlo(5,3,2);
+        for(int i = 0; i < 10000; i ++){
+            if(mcObj.insideCircle(mcObj.nextRainDrop_x(), mcObj.nextRainDrop_y())){
+                cirCount++;
+            }
+            sqrCount++;
+        }
+        int guess = cirCount * (16)/(sqrCount*16);
+
+        System.out.println(guess + ", " + cirCount + ", " + sqrCount);
     }
+
+
+
 }
